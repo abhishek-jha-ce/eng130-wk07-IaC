990f42ecc1667401629952171b0e607a4498acc9
Abhishek Jha <abhishek.jha.uk@gmail.com>
2022-11-18 09:09:39 +0000

    changes to yaml

--- provision.sh
+++ provision.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install software-properties-common -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install software-properties-common
 
 # ensure line 3 executes in the script
-sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-add-repository ppa:ansible/ansible
 
-sudo apt-get upgrade -y 
+sudo apt-get update -y 
 sudo apt-get install ansible -y
 
 #to check if setup properly
-ansible --version
+# ansible --version
 
 #to install tree -to view folder structure in a nice way
 sudo apt-get install tree
 
-cd /etc/ansible
-echo [web] >> hosts
-echo 192.168.33.10 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant >> hosts
-echo [db] >> hosts
-echo 192.168.33.11 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant >> hosts
+# Doing it manually in hosts file in this iteration - These will append the following 4 lines of code in the hosts file
+# cd /etc/ansible
+# echo [web] >> hosts
+# echo 192.168.33.10 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant >> hosts
+# echo [db] >> hosts
+# echo 192.168.33.11 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant >> hosts
